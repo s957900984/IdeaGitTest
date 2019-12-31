74a93c2fa4f0ee7e08e881613647ea583c682f6f
barefoot <barefoot@qq.com>
2019-12-31 15:39:49 +0800

    first commit

---
+++ src/com/igeek/app/App.java
@@ -0,0 +1,14 @@
+package com.igeek.app;
+
+/**
+ * @Author : FMH
+        * @Date : 2019/12/31 15:34
+        * @Description :
+        * @Modify By : 86159
+        * @Version :
+        */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hello world!");
+    }
+}
