28d611e447be5c6eb0f97399af7145fb330ff87b
barefoot <barefoot@qq.com>
2019-12-31 15:50:07 +0800

    hello

--- src/com/igeek/app/App.java
+++ src/com/igeek/app/App.java
@@ -10,5 +10,6 @@ package com.igeek.app;
 public class App {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        System.out.println("hello!");
     }
 }
